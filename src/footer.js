3460bd20ef41db3d6f61bd8a599a92f18df0e9b8
Deabeh18 <chris.deabeh@gmail.com>
2022-12-12 16:25:45 +0000

    initial commit

---
+++ src/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+
+<footer>
+    <h1>created by <a href="https://github.com/Deabeh18">Deabeh18</a></h1>
+</footer>
+
+    )
+}
